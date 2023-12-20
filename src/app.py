dbc11bdd2a4e10a789e1ab6116ce61a8ee8a7bd3
valdez-nati <nataliaval1912@gmail.com>
2023-12-20 20:35:42 -0300

    medio diario

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash  
+from flask import Flask, render_template, request, redirect, url_for, flash,jsonify,session
 from flask_mysqldb import MySQL
 from werkzeug.security import check_password_hash
 from config import config
@@ -19,7 +19,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 
 csrf= CSRFProtect(app)
-
+app.secret_key = config
 db= MySQL(app)
 login_manager_app= LoginManager(app)
 
@@ -59,10 +59,7 @@ def login():
  
     
     
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
+
         
 
 
@@ -76,8 +73,6 @@ def status_404(error):
 @app.route('/home')
 @login_required
 def home():
-    #username = current_user.username 
-
     mycursor = db.connection.cursor()
     sql = "SELECT * FROM clientes"
     mycursor.execute(sql)
@@ -111,7 +106,7 @@ def nuevocliente():
         return redirect(url_for('home'))   
 
 
-@app.route("/editarcliente",methods= ['POST', 'GET'])# tiene que lamarse igual la funcion y la url/
+@app.route("/editarcliente",methods= ['POST', 'GET'])# tiene que llamarse igual la funcion y la url/
 @login_required
 def editarcliente():
     if request.method == 'POST':
@@ -134,45 +129,152 @@ def borrarcliente(ruc):
 @app.route("/cargar/")
 @login_required
 def fact():
-    
     idcliente = request.args.get('idcliente')
-    return render_template('cargar.html', idcliente=idcliente)
+    session['idcliente'] = idcliente
+    mycursor = db.connection.cursor()
+    sql = "SELECT idplancuenta, descripcion FROM plancuentas WHERE imputable=1"
+    mycursor.execute(sql)
+    data = mycursor.fetchall()
+    mycursor.close()
+    return render_template('cargar_diario.html', idcliente=idcliente, data=data)
 
     
-@app.route("/compra/")
+@app.route("/cargar_diario/",methods= ['POST', 'GET'])
 @login_required
-def compra():
-    return render_template('fact.html')
+def cargar_diario():
+    try:
+        idcliente = session.get('idcliente')
+        fecha = request.form.get("fecha")
+        numeroasiento = request.form.get("numeroasiento")
+        descripcion = request.form.get("descripcion")
+        importe = request.form.get("importe")
+        debe = request.form.get("debe")
+        haber = request.form.get("haber")
+        cuentas = request.form.get("cuentas")
+        print("Fecha",fecha)
+        print("Numero de asiento:",numeroasiento)
+        print("Descripcion:",descripcion)
+        print("Importe:",importe)
+        print("Debe:",debe)
+        print("Haber:",haber)
+        print("Cuenta:",cuentas)
+        
+        print("CLIENTE:",idcliente)
+
+        mycursor = db.connection.cursor()
+        sql = "INSERT INTO asientoregistro (fecha,numeroasiento,descripcion,clientefk) VALUES (%s, %s, %s, %s)"
+        val = (fecha,numeroasiento,descripcion,idcliente)
+        mycursor.execute(sql, val,)
+        db.connection.commit()
+        mycursor.close()
+
+
+
+
+        return render_template('cargar_diario.html')
+    except Exception as e:
+        # En caso de error, puedes imprimir o manejar el error de alguna manera
+        print(f"Error: {str(e)}")
+        return jsonify({'success': False, 'error': str(e)})
+
 
 @app.route("/venta/")
 @login_required
 def venta():
-    return render_template('venta.html')
+    mycursor = db.connection.cursor()
 
-@app.route("/asiento_compra/",methods= ['POST', 'GET'])# tiene que lamarse igual la funcion y la url/
+    sql = """
+        SELECT 
+            'tipoventas' AS tipo, 
+            idtipoventa AS id,
+            descripcion
+        FROM tipoventas
+
+        UNION 
+
+        SELECT
+            'ideventas' AS tipo,
+            ideventa AS id, 
+            descripcion
+        FROM ideventas
+    """
+
+    mycursor.execute(sql)
+    resultados = mycursor.fetchall()
+
+    tipoventas = [r for r in resultados if r[0] == 'tipoventas']
+    ideventas = [r for r in resultados if r[0] == 'ideventas']
+
+    print(tipoventas)
+ 
+    print(ideventas)
+    return render_template('venta.html',tipoventas=tipoventas,ideventas=ideventas)
+
+
+            
+@app.route("/obtener_compra/",methods= ['POST', 'GET'])# tiene que llamarse igual la funcion y la url/
 @login_required
-def asiento_compra():
-    if request.method == 'POST':
-        selected_tipoc = request.form.get('tipoc')
-        fecha_value = request.form.get('fecha')
-        timbrado_value = request.form.get('timbrado')
-        ncomprobante_value = request.form.get('ncomprobante')
-        monto_gravado_10_value = request.form.get('monto_gravado_10')
-        monto_impuesto_10_value = request.form.get('monto_impuesto_10')
-        # Extract other values as needed
+def obtener_compra():
+    try:
 
-        # Print or use the extracted values as required
-        print(f'Tipo de Comprobantes: {selected_tipoc}')
-        print(f'Fecha: {fecha_value}')
-        print(f'Timbrado: {timbrado_value}')
-        print(f'Numero de comprobante: {ncomprobante_value}')
-        print(f'Monto Gravado 10%: {monto_gravado_10_value}')
-        print(f'Monto Impuesto 10%: {monto_impuesto_10_value}')
-        return redirect(url_for('home'))
-    
+        #trae el json del javascript
+        data = request.get_json()
+        # el json por separado
+        idcliente = data.get('idcliente')
+        total10 = data.get('total10')
+        total5 = data.get('total5')
+        totalIVA = data.get('totalIVA')
+        montoExento= data.get('montoExento')
+        montoGravado10 = data.get('montoGravado10')
+        montoGravado5 = data.get('montoGravado5')
+        totalComprobante= data.get('totalComprobante')
+      
+        # print("ID Cliente:", idcliente)
+        # print("Tototal10:", total10)
+        # print("Total5:", total5)
+        # print("Gravado5:", montoGravado5)
+        # print("Gravado10:", montoGravado10)
+        # print("Exento:", montoExento)
+
+        # print("Total IVA:",totalIVA)
+        # print("TOTAL:", totalComprobante)
+        
+        
+        session['datos_compra'] = {
+            'idcliente': idcliente,
+            'total10': total10,
+            'total5': total5,
+            'totalIVA': totalIVA,
+            'montoExento': montoExento,
+            'montoGravado10': montoGravado10,
+            'montoGravado5': montoGravado5,
+            'totalComprobante': totalComprobante
+        }
+        return redirect(url_for('cargar_compra'))
+
+
+   
+       # return jsonify({'success': True})
+
+    except Exception as e:
+        # En caso de error, puedes imprimir o manejar el error de alguna manera
+        print(f"Error: {str(e)}")
+        return jsonify({'success': False, 'error': str(e)})
+
+            
+            
+            
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
     app.config.from_object(config['development'])
     csrf.init_app(app)
     app.register_error_handler(401, status_401)
     app.register_error_handler(404, status_404)
+    
+
     app.run()
--- src/static/js/compra.js
+++ src/static/js/compra.js
@@ -1,35 +1,73 @@
-// Envuelve tu código en un bloque document ready
+
+// Función para limpiar el formulario
+function limpiarFormulario() {
+    document.getElementById("miFormulario").reset();
+    // Limpia los campos específicos 
+    $("#montoGravado10").val(0);
+    $("#montoGravado5").val(0);
+    $("#montoExento").val(0);
+
+    // Limpia los resultados de los totales
+    $("#total10").text("0.00");
+    $("#total5").text("0.00");
+    $("#totalIVA").text("0.00");
+    $("#totalComprobante").text("0");
+}
+
+
 $(document).ready(function() {
+    //aca tambien se necesita el token para poder envia los datos a python
+    var csrfToken = document.getElementById('csrf_token').value;
+    console.log(csrfToken);
+    var tipoOperacion = "";
+        // Evento de clic en un botón de compra
+    $("#compraBoton").on("click", function() {
+        tipoOperacion = "compra";
+        // Actualiza la URL a la que se enviarán los datos
+        urlEnviarDatos = "/obtener_compra/";
+    });
 
-    // Función para cambiar interruptores
-    function toggleSwitch(clickedSwitch) {
-        const allSwitches = document.querySelectorAll('.switch input');
-
-        allSwitches.forEach(function(switchInput) {
-            if (switchInput !== clickedSwitch) {
-                switchInput.checked = false;
-            }
-        });
+    // Evento de clic en un botón de venta
+    $("#ventaBoton").on("click", function() {
+        tipoOperacion = "venta";
+        // Actualiza la URL a la que se enviarán los datos
+        urlEnviarDatos = "/obtener_datos_venta/";
+    });
+    // Obtener parámetro de la URL
+    function getParameterByName(name) {
+        const url = window.location.href;
+        name = name.replace(/[\[\]]/g, '\\$&');
+        
+        const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+        
+        if (!results) return null;
+        if (!results[2]) return '';
+        
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
     }
+    
 
-    // Función para limpiar el formulario
-    function limpiarFormulario() {
-        document.getElementById("miFormulario").reset();
-    }
 
     // Función para calcular los totales
     function calcularTotales() {
-        var montoGravado10 = parseFloat($("#montoGravado10").val()) || 0;
-        var impuestoGravado10 = montoGravado10 * 0.1;
+        var total10= 0;     
+        var total5= 0;
+        var montoGravado10 = parseInt($("#montoGravado10").val()) || 0;
+        var impuestoGravado10 = montoGravado10 /11;
         var montoGravado5 = parseFloat($("#montoGravado5").val()) || 0;
-        var impuestoGravado5 = montoGravado5 * 0.05;
+        var impuestoGravado5 = montoGravado5 /21;
         var montoExento = parseFloat($("#montoExento").val()) || 0;
 
+        total10 = total10+impuestoGravado10;
+        total5 = total5 + impuestoGravado5;
         var totalIVA = impuestoGravado10 + impuestoGravado5;
-        var totalComprobante = montoGravado10 + impuestoGravado10 + montoGravado5 + impuestoGravado5 + montoExento;
-
+        var totalComprobante = parseInt(montoGravado10 + montoGravado5 + montoExento);
+        $("#total10").text(total10.toFixed(2));
+        $("#total5").text(total5.toFixed(2));
         $("#totalIVA").text(totalIVA.toFixed(2));
-        $("#totalComprobante").text(totalComprobante.toFixed(2));
+        $("#totalComprobante").text(totalComprobante);
+        
     }
 
     // Configura eventos de cambio en los campos de entrada para recalcular los totales
@@ -38,12 +76,58 @@ $(document).ready(function() {
     // Inicializa los totales al cargar la página
     calcularTotales();
 
-    // Obtén el ID del cliente de la URL
+    // Obténer el idcliente de la URL
     var idcliente = getParameterByName('idcliente');
     console.log('ID del cliente:', idcliente);
 
-    // Agrega aquí el resto de tu código, si lo tienes.
+    // Evento de clic en un botón (ficticio) para enviar datos a Python
+    $("#enviarDatosBoton").on("click", function() {
+        //if (tipoOperacion === "compra") {
+          //  enviarDatos("/obtener_compra/");
+        //} else if (tipoOperacion === "venta") {
+          //  enviarDatos("/obtener_venta/");
+        //}
+    
+
+        // Aquí podrías construir un objeto con los datos que deseas enviar
+        var datosAEnviar = {
+            idcliente: idcliente,
+            montoGravado10: $("#montoGravado10").text(),
+            total10: $("#total10").text(),
+            montoGravado5: $("#montoGravado5").text(),
+            montoExento: $("#montoExento").text(),
+            total5: $("#total5").text(),
+            totalIVA: $("#totalIVA").text(),
+            totalComprobante: $("#totalComprobante").text()
+           
+        };
+        
+            $.ajax({
+                type: "POST",
+                url: "/obtener_compra/",
+                data: JSON.stringify(datosAEnviar),
+                contentType: "application/json",
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader("X-CSRFToken", csrfToken);
+                },
+                success: function(response) {
+                    console.log('Respuesta del servidor:', response);
+                },
+                error: function(xhr, status, error) {
+                    console.error('Error en la solicitud Ajax:', error);
+                    console.log('Estado de la solicitud:', status);
+                    console.log('Respuesta del servidor:', xhr.responseText);
+                }
+            });
+        
+        
+       
+    });
+   
+    
+
+    $("#limpiarBoton").on("click", limpiarFormulario);
+    // Click event for an imaginary button to send data
 
 });
 
-// Agrega aquí el resto de tu código, si lo tienes.
